b394b6a0ae432fcaa5a28c14cd30a9891ed0ec98
Ben Garbacz <bentgarbacz@gmail.com>
2024-07-15 14:37:55 -0400

    fixed spell assignment box sizing and positioning

--- Assets/Scripts/Managers/SpellAssignmentManager.cs
+++ Assets/Scripts/Managers/SpellAssignmentManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System;
 
 public class SpellAssignmentManager : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class SpellAssignmentManager : MonoBehaviour
     [SerializeField] private GameObject spellIconPrefab;
     [SerializeField] private GameObject spellIconGrid;
 
-    void Start()
+    void Awake()
     {
         
         spellAssignmentContainerRect = GetComponent<RectTransform>();
@@ -21,6 +22,10 @@ public class SpellAssignmentManager : MonoBehaviour
 
     void OnEnable()
     {
+
+        int iconCount = 1 + pc.knownSpells.Keys.Count; //one icon for clearing spell plus an icon for each known spell
+        int rectDimensionX = 20 + 75 * Math.Min(iconCount, 3); //20 units for padding, 75 * number of columns, max 3 columns
+        int rectDimensionY = 20 + 75 * ((iconCount + 2) / 3); //20 units for padding, 75 * number of rows
 
         GameObject clearSpellIcon = Instantiate(spellIconPrefab, spellIconGrid.transform);
         clearSpellIcon.GetComponent<SpellSelector>().SetSpell("", currentSpellSlot, currentTooltip);
@@ -32,7 +37,7 @@ public class SpellAssignmentManager : MonoBehaviour
             newSpellIcon.GetComponent<SpellSelector>().SetSpell(spell, currentSpellSlot, currentTooltip);
         }
 
-        //spellAssignmentContainerRect.sizeDelta = spellIconGrid.GetComponent<RectTransform>().sizeDelta + new Vector2(10, 10);
+        spellAssignmentContainerRect.sizeDelta = new Vector2(rectDimensionX, rectDimensionY);
     }
 
     void OnDisable()
