540ddf3129251e6fadb1fcf2293c7ec470b6b514
Ben Garbacz <bentgarbacz@gmail.com>
2024-10-13 01:58:18 -0400

    Fixed item transfering bugs and polished item dragging mechanics

--- Assets/Scripts/UI Scripts/TakeItem.cs
+++ Assets/Scripts/UI Scripts/TakeItem.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TakeItem : MonoBehaviour
+public class TakeItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     private InventoryManager im;
     public AudioSource audioSource;
     private ItemSlot itemSlot;
+    private ItemDragManager idm;
 
     void Start()
     {
@@ -15,7 +17,8 @@ public class TakeItem : MonoBehaviour
         itemSlot = transform.parent.gameObject.GetComponent<ItemSlot>();
 
         im = GameObject.Find("System Managers").GetComponent<InventoryManager>();
-        audioSource = GameObject.Find("CanvasHUD").GetComponent<AudioSource>(); 
+        audioSource = GameObject.Find("CanvasHUD").GetComponent<AudioSource>();
+        idm = GameObject.Find("System Managers").GetComponent<UIActiveManager>().itemDragContainer.GetComponent<ItemDragManager>(); 
     }
 
     public void Click()
@@ -23,5 +26,18 @@ public class TakeItem : MonoBehaviour
     
         audioSource.Play();
         im.TakeItem(itemSlot);
+        idm.paused = true;
+    }
+
+     public void OnPointerEnter(PointerEventData eventData)
+    {
+
+        idm.paused = true;
+    }
+
+     public void OnPointerExit(PointerEventData eventData)
+    {
+
+        idm.paused = false;
     }    
 }
